625ab677d4935b8b66d65900e555226e2da5aa93
roamiri <amiriruhollah@gmail.com>
2018-01-31 16:09:59 -0700

    no parallelizing

--- parallel_run.m
+++ parallel_run.m
@@ -2,17 +2,18 @@
 %                     Main Loop Runner in parallel:
 %   
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function parallel_run(pref_poolSize)
-parpool(pref_poolSize)
+function parallel_run()
+% parpool(pref_poolSize)
 
 ts = 4e4; % table size
 iter = [ts, 10*ts, 20*ts, 50*ts, 100*ts];
 
-parfor_progress(5);
- parfor i=1:5
+% parfor_progress(5);
+ for i=1:5
+    fprintf('iteration = %d\n', iter(i));
     WF_RL_1(iter(i));
-    pause(rand);
-    parfor_progress;
+%     pause(rand);
+%     parfor_progress;
  end
- parfor_progress(0); % Clean up
+%  parfor_progress(0); % Clean up
 end
