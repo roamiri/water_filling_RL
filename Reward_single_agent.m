32b07bfc50fb4c1dc9839deb615d0672ed6508bc
roamiri <amiriruhollah@gmail.com>
2018-01-31 21:28:20 -0700

    bugfix

--- Reward_single_agent.m
+++ Reward_single_agent.m
@@ -2,9 +2,9 @@ function R = Reward_single_agent(PA, Pmax)
 %     punish = 0;
     punish = abs(sum(PA.P)-Pmax);
     
-    if dd>0
-        punish = log2(dd);
-    end
+%     if dd>0
+%         punish = log2(dd);
+%     end
     
     R = 0;
     for i=1:size(PA.P,2)
